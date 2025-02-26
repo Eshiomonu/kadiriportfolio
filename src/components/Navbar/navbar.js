2306e5cbed4b32ef20e2faad4cd704be1df6e5a0
kadirisunday <kadirisunday212@gmail.com>
2025-02-27 00:43:14 +0100

    Added navbar content

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./navbar.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
 
 const navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <nav className="">
+      <img src={logo} alt="logo" className="logo" />
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Name</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+        <Link className="desktopMenuListItem">Clients</Link>
+      </div>
+      <button className="desktopMenuBtn">
+        <img src="" alt="" className="desktopMenuImg" />
+        Contact Me
+      </button>
+    </nav>
+  );
 };
 
 export default navbar;
