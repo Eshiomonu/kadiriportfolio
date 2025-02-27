44d30cc454d9de31e599dd2265f3f854b094b149
kadirisunday <kadirisunday212@gmail.com>
2025-02-27 13:36:13 +0100

    skills section content done and added the assets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar/navbar";
 import Intro from "./components/Intro/intro";
+import Skills from "./components/Skills/skills";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Intro />
+      <Skills />
     </div>
   );
 };
